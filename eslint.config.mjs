88023676eead11eeac0bbcbbdce95260f30c87ea
Sanitar2000 <sanitar2000@bk.ru>
2026-03-29 15:00:28 +0400

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,42 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+// @ts-check
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import jest from 'eslint-plugin-jest'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  // DOC: https://typescript-eslint.io/getting-started#additional-configs
+  // ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    ignores: ['reports']
+  },
+  {
+    files: ['scripts/**/*.zx.js'],
+    globals: {
+      $: true,
+      fs: true
+    }
+  },
+  {
+    files: ['**/*.js'],
+    extends: [tseslint.configs.disableTypeChecked]
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn' // 'warn'
+    }
+  },
+  // DOC: https://www.npmjs.com/package/eslint-plugin-jest
+  {
+    files: ['test/**', 'setup-jest.js'],
+    ...jest.configs['flat/recommended']
+  }
+)
